ea631a80d1fae9cd70140e1935add63f60049343
keiy_ <75249966+keiyakeiya@users.noreply.github.com>
2023-09-29 15:48:44 +0900

    update

---
+++ demoCode/javascritp.js
@@ -0,0 +1,15 @@
+let a = 1;
+const b = true;
+const c = 'Hello';
+
+// # element
+// variable
+// array
+// function
+// arrow
+// class
+// method
+// string
+// boolean
+// number
+// if,for, while
